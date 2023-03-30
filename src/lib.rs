734dc7247ecbc898d9fbbe819276712c782674cc
Nazım Can Altınova <canaltinova@gmail.com>
2023-03-30 21:22:48 +0200

    Put the startup related code to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,4 @@
 pub mod configuration;
 pub mod routes;
+pub mod startup;
 pub mod symbol_manager;
-
-use std::{net::TcpListener, sync::Arc};
-
-use crate::configuration::Settings;
-use crate::routes::{asm_v1, greet, health_check, symbolicate_v5};
-use crate::symbol_manager::create_symbol_manager;
-use actix_cors::Cors;
-use actix_web::{dev::Server, web, App, HttpServer};
-
-pub fn run(listener: TcpListener, settings: Settings) -> Result<Server, std::io::Error> {
-    let symbol_manager = Arc::new(create_symbol_manager(settings));
-    let server = HttpServer::new(move || {
-        let cors = Cors::default()
-            .allow_any_origin()
-            .allowed_methods(vec!["GET", "POST", "OPTION"])
-            .allow_any_header();
-        App::new()
-            .wrap(cors)
-            .route("/", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-            .route("/symbolicate/v5", web::post().to(symbolicate_v5))
-            .route("/asm/v1", web::post().to(asm_v1))
-            .app_data(web::Data::new(symbol_manager.clone()))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
